7f5821a82f7256a97d1600fb9614549f94d93a51
Romain Beaumont <romain.rom1@gmail.com>
2017-06-29 17:46:33 +0200

    add launcher_download class that act like the launcher :
    * download the files downloaded by the launcher : assets, libraries and jars
    * place them inside the minecraft dir
    * don't download if the sha1 and size of existing files is correct

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports={
   Wrap:require("./lib/wrap"),
   download:require("./lib/download").downloadServer,
   downloadServer:require("./lib/download").downloadServer,
-  downloadClient:require("./lib/download").downloadClient
+  downloadClient:require("./lib/download").downloadClient,
+  LauncherDownload:require("./lib/launcher_download")
 };
