69940005678b0058fe358ff5e0aa8c402d8858e7
Romain Beaumont <romain.rom1@gmail.com>
2017-07-19 00:37:04 +0200

    client_control based on robotjs : login then jump then say hi then closes

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-module.exports = {
-  Wrap: require('./lib/wrap_server'),
-  WrapServer: require('./lib/wrap_server'),
-  WrapClient: require('./lib/wrap_client'),
-  download: require('./lib/download').downloadServer,
-  downloadServer: require('./lib/download').downloadServer,
-  downloadClient: require('./lib/download').downloadClient,
-  LauncherDownload: require('./lib/launcher_download')
+const debug = require('debug')('minecraft-wrap');
+
+let ClientControl;
+try {
+  ClientControl=require('./lib/client_control');
 }
+catch(err) {
+  debug(err);
+}
+
+module.exports={
+  Wrap:require("./lib/wrap_server"),
+  WrapServer:require("./lib/wrap_server"),
+  WrapClient:require("./lib/wrap_client"),
+  download:require("./lib/download").downloadServer,
+  downloadServer:require("./lib/download").downloadServer,
+  downloadClient:require("./lib/download").downloadClient,
+  LauncherDownload:require("./lib/launcher_download"),
+  ClientControl
+};
