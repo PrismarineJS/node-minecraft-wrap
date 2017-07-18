9ff7c688876906af75bad5d0758796ef888cedb5
Romain Beaumont <romain.rom1@gmail.com>
2017-07-18 15:42:54 +0200

    improve download script a little

--- examples/exampleDownload.js
+++ examples/exampleDownload.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 
-var wrap=require("../");
-var path = require('path');
+const wrap=require("../");
+const path = require('path');
 if(process.argv.length <4 || process.argv.length >5) {
   console.log("Usage : node exampleDownload.js <version> <jar_file> [<client|server>]");
   process.exit(0);
 }
 
-var version=process.argv[2];
-var jarFile=path.isAbsolute(process.argv[3]) ? process.argv[3] : path.join(process.cwd(), process.argv[3]);
-var downloadServer=!process.argv[4] || process.argv[4]=="server";
+const version=process.argv[2];
+const jarFile=path.isAbsolute(process.argv[3]) ? process.argv[3] : path.join(process.cwd(), process.argv[3]);
+const downloadServer=!process.argv[4] || process.argv[4]!=="server";
 
 wrap["download"+(downloadServer ? "Server" : "Client")](version,jarFile,function(err){
   if(err) {
