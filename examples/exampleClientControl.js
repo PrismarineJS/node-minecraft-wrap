b93e812c97a36392c81a1555e3df96581fcae6bd
Romain Beaumont <romain.rom1@gmail.com>
2021-04-18 14:33:04 +0000

    standard

--- examples/exampleClientControl.js
+++ examples/exampleClientControl.js
@@ -1,60 +1,62 @@
 #!/usr/bin/env node
 
-const WrapClient=require("../").WrapClient;
-const ClientControl=require("../").ClientControl;
-const path = require('path');
+const WrapClient = require('../').WrapClient
+const ClientControl = require('../').ClientControl
+const path = require('path')
 
-if(process.argv.length < 0) {
-  console.log("Usage : node exampleClientControl.js [<minecraft dir>] [<version>] [<username>] [<password>] [<stop>]");
-  process.exit(0);
+if (process.argv.length < 0) {
+  console.log('Usage : node exampleClientControl.js [<minecraft dir>] [<version>] [<username>] [<password>] [<stop>]')
+  process.exit(0)
 }
 
-const version=process.argv[3];
-const dir=process.argv[2] ? path.isAbsolute(process.argv[2]) ? process.argv[2] : path.join(process.cwd(), process.argv[2]) : null;
+const version = process.argv[3]
+const dir = process.argv[2] ? path.isAbsolute(process.argv[2]) ? process.argv[2] : path.join(process.cwd(), process.argv[2]) : null
 
-const vClient=new WrapClient(dir,version);
-const username=process.argv[4];
-const password=process.argv[5];
-const stop=true;
-const clientControl=new ClientControl(vClient);
+const vClient = new WrapClient(dir, version)
+const username = process.argv[4]
+const password = process.argv[5]
+const stop = true
+const clientControl = new ClientControl(vClient)
 
-vClient.on('line',function(line){
-  console.log(line);
-});
+vClient.on('line', function (line) {
+  console.log(line)
+})
 
 Promise.resolve()
-  .then(() => vClient.auth(username,password))
-  .then(() => console.log("User logged in !"))
+  .then(() => vClient.auth(username, password))
+  .then(() => console.log('User logged in !'))
   .then(() => vClient.prepare())
-  .then(() => console.log("Client prepared !"))
+  .then(() => console.log('Client prepared !'))
   .then(() => vClient.start())
   .then(() => {
-    console.log("Client Started !");
+    console.log('Client Started !')
   })
   .then(() => {
-    return clientControl.login("localhost")
+    return clientControl.login('localhost')
   })
   .then(() => {
-    console.log("Login successful");
-    return clientControl.jump();
+    console.log('Login successful')
+    return clientControl.jump()
   })
   .then(() => {
-    console.log("Jump successful");
-    return clientControl.chat("hiiiiii");
+    console.log('Jump successful')
+    return clientControl.chat('hiiiiii')
   })
   .then(() => {
-    if(stop) setTimeout(() => {
-      vClient.stop()
-        .then(() => {
-          console.log("Client Stopped !");
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    },3000)
+    if (stop) {
+      setTimeout(() => {
+        vClient.stop()
+          .then(() => {
+            console.log('Client Stopped !')
+          })
+          .catch((err) => {
+            console.log(err)
+          })
+      }, 3000)
+    }
   })
   .catch((err) => {
-    console.log(err);
-    vClient.stop();
-    process.exit();
-  });
+    console.log(err)
+    vClient.stop()
+    process.exit()
+  })
--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-const debug = require('debug')('minecraft-wrap');
+const debug = require('debug')('minecraft-wrap')
 
-let ClientControl;
+let ClientControl
 try {
-  ClientControl=require('./lib/client_control');
-}
-catch(err) {
-  debug(err);
+  ClientControl = require('./lib/client_control')
+} catch (err) {
+  debug(err)
 }
 
-module.exports={
-  Wrap:require("./lib/wrap_server"),
-  WrapServer:require("./lib/wrap_server"),
-  WrapClient:require("./lib/wrap_client"),
-  download:require("./lib/download").downloadServer,
-  downloadServer:require("./lib/download").downloadServer,
-  downloadClient:require("./lib/download").downloadClient,
-  LauncherDownload:require("./lib/launcher_download"),
+module.exports = {
+  Wrap: require('./lib/wrap_server'),
+  WrapServer: require('./lib/wrap_server'),
+  WrapClient: require('./lib/wrap_client'),
+  download: require('./lib/download').downloadServer,
+  downloadServer: require('./lib/download').downloadServer,
+  downloadClient: require('./lib/download').downloadClient,
+  LauncherDownload: require('./lib/launcher_download'),
   ClientControl
-};
+}
