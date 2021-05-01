4e8f579c77ec71fedd7cab2a73b7f0f9cb61479a
u9g <43508353+u9g@users.noreply.github.com>
2021-05-01 19:19:26 -0400

    remove debug line

--- examples/exampleBedrockDownload.js
+++ examples/exampleBedrockDownload.js
@@ -6,5 +6,4 @@ if (process.argv.length < 4 || process.argv.length > 5) {
   console.log('Usage : node exampleBedrockServer.js <version> <outputFolder>')
   process.exit(0)
 }
-console.log(process.argv)
 wrap.downloadBedrockServer(process.platform, process.argv[2], process.argv[3])
