1e0f301534bec3d4027a0c406bd5d4fb46e7d0d7
u9g <git@u9g.dev>
2022-06-19 18:40:09 -0400

    Test 1.19 too

--- test/wrap_server.js
+++ test/wrap_server.js
@@ -6,56 +6,59 @@ const path = require('path')
 const MC_SERVER_PATH = path.join(__dirname, 'server')
 const MC_SERVER_JAR = path.join(__dirname, 'server.jar')
 const fs = require('fs')
+const testedVersions = require('./tested_versions.json')
 
-describe('server_session', function () {
-  this.timeout(10 * 60 * 1000)
-  before((done) => {
-    downloadServer('1.13', MC_SERVER_JAR, err => {
-      if (err) return done(err)
+for (const version of testedVersions) {
+  describe(`server_session ${version}`, function () {
+    this.timeout(10 * 60 * 1000)
+    before((done) => {
+      downloadServer(version, MC_SERVER_JAR, err => {
+        if (err) return done(err)
+        done()
+      })
+    })
+    after((done) => {
+      fs.unlinkSync(MC_SERVER_JAR)
       done()
     })
-  })
-  after((done) => {
-    fs.unlinkSync(MC_SERVER_JAR)
-    done()
-  })
 
-  it('start and stop the server', function (done) {
-    const vServer = new WrapServer(MC_SERVER_JAR, MC_SERVER_PATH)
+    it('start and stop the server', function (done) {
+      const vServer = new WrapServer(MC_SERVER_JAR, MC_SERVER_PATH)
 
-    vServer.on('line', function (line) {
-      console.log(line)
-    })
+      vServer.on('line', function (line) {
+        console.log(line)
+      })
 
-    vServer.startServer({ 'server-port': 25569 },
-      function (err) {
-        if (err) {
-          console.log(err)
-          done(err)
-          return
-        }
-        console.log('Server Started !')
+      vServer.startServer({ 'server-port': 25569 },
+        function (err) {
+          if (err) {
+            console.log(err)
+            done(err)
+            return
+          }
+          console.log('Server Started !')
 
-        setTimeout(function () {
-          vServer.stopServer(function (err) {
-            if (err) {
-              console.log(err)
-              done(err)
-              return
-            }
-            console.log('Server Stopped !')
-
-            vServer.deleteServerData(function (err) {
+          setTimeout(function () {
+            vServer.stopServer(function (err) {
               if (err) {
                 console.log(err)
                 done(err)
                 return
               }
-              console.log('Server data deleted !')
-              done()
+              console.log('Server Stopped !')
+
+              vServer.deleteServerData(function (err) {
+                if (err) {
+                  console.log(err)
+                  done(err)
+                  return
+                }
+                console.log('Server data deleted !')
+                done()
+              })
             })
-          })
-        }, 3000)
-      })
+          }, 3000)
+        })
+    })
   })
-})
+}
