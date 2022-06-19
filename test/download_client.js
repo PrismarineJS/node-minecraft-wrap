086bcebe07ab412a8f3f9c0b85c02f10ec936bf7
u9g <git@u9g.dev>
2022-06-19 19:04:56 -0400

    fix(download): Fix etag not md5 on all downloads (#69)
    * Remove etag download
    
    * Implement suggestion
    
    * Test 1.19 too
    
    * Don't delete file manually, thats what deleteServerData() does

--- test/download_client.js
+++ test/download_client.js
@@ -2,22 +2,25 @@
 
 const minecraftWrap = require('../')
 const fs = require('fs')
+const testedVersions = require('./tested_versions.json')
 
-describe('Download', function () {
-  this.timeout(10 * 60 * 1000)
-  it('Can download the client', done => {
-    minecraftWrap.downloadClient('1.13', '1.13-client.jar', err => {
-      if (err) return done(err)
-      fs.unlinkSync('1.13-client.jar')
-      done()
+for (const version of testedVersions) {
+  describe(`Download ${version}`, function () {
+    this.timeout(10 * 60 * 1000)
+    it('Can download the client', done => {
+      minecraftWrap.downloadClient(version, `${version}-client.jar`, err => {
+        if (err) return done(err)
+        fs.unlinkSync(`${version}-client.jar`)
+        done()
+      })
+    })
+
+    it('Can download the server', done => {
+      minecraftWrap.downloadServer(version, `${version}-server.jar`, err => {
+        if (err) return done(err)
+        fs.unlinkSync(`${version}-server.jar`)
+        done()
+      })
     })
   })
-
-  it('Can download the server', done => {
-    minecraftWrap.downloadServer('1.13', '1.13-server.jar', err => {
-      if (err) return done(err)
-      fs.unlinkSync('1.13-server.jar')
-      done()
-    })
-  })
-})
+}
